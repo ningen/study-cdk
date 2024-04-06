e53ee3599506fdea97db86bc9fabe192e85a41a1
ningen <ningen361@gmail.com>
2024-04-06 17:40:25 +0900

    hello lambda

--- lib/study-cdk-stack.ts
+++ lib/study-cdk-stack.ts
@@ -1,19 +1,14 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
-export class StudyCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class StudyCdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'StudyCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'StudyCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler"
+    })
   }
 }
