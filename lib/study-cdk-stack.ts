c4af2b3e2a905925b2d01b6f8ba868d37da75776
ningen <ningen361@gmail.com>
2024-04-07 22:37:26 +0900

    added fitcounter

--- lib/study-cdk-stack.ts
+++ lib/study-cdk-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import { HitCounter } from "./hitcounter";
 
 export class StudyCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -8,7 +10,15 @@ export class StudyCdkStack extends cdk.Stack {
     const hello = new lambda.Function(this, "HelloHandler", {
       runtime: lambda.Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset("lambda"),
-      handler: "hello.handler"
-    })
+      handler: "hello.handler",
+    });
+
+    const hellowWithCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: hello,
+    });
+
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hellowWithCounter.handler,
+    });
   }
 }
